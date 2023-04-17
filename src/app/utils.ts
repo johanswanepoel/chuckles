567f817f422674d21f1b9790cd2e78fb5b6a50ba
johanswanepoel <johanswanepoel@users.noreply.github.com>
2023-04-17 15:14:25 +0000

    deploy: b3175c13a296fc3636bde8f2ab1575e1dc620071

--- src/app/utils.ts
+++
@@ -1,7 +0,0 @@
-export const parseJSON = <T>(jsonString: string): T => {
-    return JSON.parse(jsonString)
-}
-
-export const stringifyJSON = <T>(data: T): string => {
-    return JSON.stringify(data)
-}
--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    chuckNorrisJokesEndPointUrl: 'https://api.chucknorris.io',
-}
